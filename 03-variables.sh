65ff3975b81242c291c70c4484a22804d9c965fa
siva <sivasravan32@gmail.com>
2025-08-28 20:16:16 +0530

    hello world created

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1=trump
+person2=musk
+
+echo "$person1: hi $person2? I am $person1"
+echo "$person2: hi $person1. how do u know i am $person2"
+echo "$person1: i saw ur photo on tv $person2"
+echo "$person2: ok $person1.thanks"
