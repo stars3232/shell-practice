8f676575adb7cb9c3e9426b7ae63dc59caa921d2
siva <sivasravan32@gmail.com>
2025-09-01 01:50:12 +0530

    variables started

---
+++ 04-variables_1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1=$1
+person2=$2
+
+echo "$person1: hi $person2? I am $person1"
+echo "$person2: hi $person1. how do u know i am $person2"
+echo "$person1: i saw ur photo on tv $person2"
+echo "$person2: ok $person1.thanks"
