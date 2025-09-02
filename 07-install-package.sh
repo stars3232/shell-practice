7c2d0b1a198a83cea422a4ad19bf4d2720ceeca5
siva <sivasravan32@gmail.com>
2025-09-03 02:01:57 +0530

    addition started

---
+++ 07-install-package.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+userid=$(id -u)
+
+if [ $userid -eq 0 ]
+then
+    echo "u r running with root access u can go forward"
+else
+    echo "please run as root user"
+    exit 1
+fi
+
+dnf list installed mysql
+
+if [ $? -eq 0 ]
+then
+echo "mysql is already installed"
+exit 1
+else
+echo "mysql is not installed, going to install it"
+
+      dnf install mysql -y
+
+      if [ $? -eq 0 ]
+      then 
+          echo "installation successful"
+      else
+          echo "mysql installation is failure"
+          exit 1
+      fi  
+
+fi       
+
+# dnf install mysql -y
+
+# if [ $? -eq 0 ]
+# then 
+#     echo "installation successful"
+# else
+#     echo "mysql installation is failure"
+#     exit 1
+# fi    
