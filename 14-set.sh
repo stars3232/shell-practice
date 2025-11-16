4770c919dc97bcccae03abc6e0e447cb116154ca
siva <sivasravan32@gmail.com>
2025-11-16 23:49:25 +0530

    shell practice

--- 14-set.sh
+++ 14-set.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-#set -e
+set -e
+
+failure()
+{
+    echo "failed at line $1, $2"
+}
+trap 'failure "${LINENO}" "${BASH_CAMMAOND}"' ERR
 
 echo "Hello Good morning"
 echooo "This  is an error"
