748f9069d007917a2f7af1ef6da2a43c05eba1da
siva <sivasravan32@gmail.com>
2026-01-06 19:54:16 +0530

    shell practice

--- 18-zipanddelete.sh
+++ 18-zipanddelete.sh
@@ -1,64 +1,73 @@
-#/bin/bash
+#!/bin/bash
 
 USER_ID=$(id -u)
 SOURCE_DIR=$1
 TARGET_DIR=$2
-DAYS=$(3:-14)
+DAYS=${3:-14}
 
-LOG_FILE="/var/log/shell-script-log"
+LOG_FOLDER="/var/log/shell-script-log"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOG_FILE/$SCRIPT_NAME.log"
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
 
-mkdir -p $LOG_FILE
+mkdir -p $LOG_FOLDER
 
 if [ $USER_ID -eq 0 ]
 then
-    echo "You r running with root access. u can ove forward"
+    echo "You r running with root access. u can ove forward" | tee -a $LOG_FILE
 else 
-    echo "Please run as root user"
+    echo "Please run as root user" | tee -a $LOG_FILE
     exit 1
 fi
 
 USAGE()
 {
-    echo "Scrpit should be given SOURCE_DIR, TARGET_DIR, DAYS"
+    echo "Scrpit should be given SOURCE_DIR, TARGET_DIR, DAYS" | tee -a $LOG_FILE
+    exit 1
 }
 
 if [ $# -lt 2 ]
 then
 USAGE
-exit 1
 fi
 
 if [ -d $SOURCE_DIR ]
 then
-echo " $SOURCE_DIR exists"
+echo " $SOURCE_DIR exists" | tee -a $LOG_FILE
 else
-echo " $SOURCE_DIR does not exists"
+echo " $SOURCE_DIR does not exists" | tee -a $LOG_FILE
 exit 1
 fi
 
 if [ -d $TARGET_DIR ]
 then
-echo " $TARGET_DIR exists"
+echo " $TARGET_DIR exists" | tee -a $LOG_FILE
 else
-echo " $TARGET_DIR does not exists"
+echo " $TARGET_DIR does not exists" | tee -a $LOG_FILE
 exit 1
 fi
 
 FILES=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS)
 
-if [ -f $FILES -ne 0 ]
+if [ ! -z $FILES  ]
 then
-    echo "files found $FILES"
-    ZIP_FILE="$TARGET_DIR/app-logs.zip
-    find $SOURCE_DIR -name "*.log" -mtime +$DAYS | zip
-    if [ -f ]
+    echo "files found $FILES" | tee -a $LOG_FILE
+    ZIP_FILE="$TARGET_DIR/app-logs.zip"
+    find $SOURCE_DIR -name "*.log" -mtime +$DAYS | zip -@ "$ZIP_FILE"
 
-    while IFS= read -s filepath
+    if [ -f $ZIP_FILE ]
+    then
+    echo "logfiles are zipped succesfully" | tee -a $LOG_FILE
+    while IFS= read -r filepath
     do
-      rm -rf $filepath
+    echo "deleting file $filepath" | tee -a $LOG_FILE
+    rm -rf $filepath
     done <<< $FILES
+    echo "files older than $DAYS are successfully removed" | tee -a $LOG_FILE
+    else 
+    echo "zipping logfiles is failure" | tee -a $LOG_FILE
+    exit 1
+    fi
+
 else
-    echo "source_dir is empty"
+    echo "source_dir is empty" | tee -a $LOG_FILE
 fi
